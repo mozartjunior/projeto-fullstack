1bce0c12b641c1b622f968acf84433d89c6cef1d
Mozart Silva <MozartSJ@st.corp>
2026-03-25 15:43:15 -0400

    feature: adicionado app.ts e modificado arquivo para a primeira rota /

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,12 @@
 import 'reflect-metadata';
+import app from './app.js';
 import express from 'express';
 import 'dotenv/config';
 import { appDataSource } from './database/appDataSource.js';
 // import routes from './routes/index.js';
 // import { errorHandler } from './middlewares/errorHandler.js';
 
-const app = express();
+// const app = express();
 const PORT = process.env.PORT ?? 5432;
 
 app.use(express.json());
@@ -14,11 +15,12 @@ app.use(express.json());
 
 appDataSource.initialize()
         .then(() => {
-            console.log('banco conectado')
+            console.log('Conexão com o banco de dados estabelecida com sucesso!');
+            
             app.listen(PORT, () => {
             console.log(`Server is running in PORT: ${PORT}`)
-        })
+            });
         })
         .catch((error) => {
-            console.log(error)
+            console.log("Erro ao inciar aplicacao: ", error)
         })
