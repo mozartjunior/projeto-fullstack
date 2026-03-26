c527731647a73da81e3d3d8511ed274f2f7afc1e
Mozart Junior <mozartjrs@gmail.com>
2026-03-25 23:39:12 -0300

    Test: POST no /equipamento PASS!

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,14 +3,15 @@ import app from './app.js';
 import express from 'express';
 import 'dotenv/config';
 import { appDataSource } from './database/appDataSource.js';
+import equipamentoRoutes from './routes/equipamento.routes.js'
 // import routes from './routes/index.js';
 // import { errorHandler } from './middlewares/errorHandler.js';
 
 // const app = express();
-const PORT = process.env.PORT ?? 5432;
+const PORT = process.env.PORT ?? 6060;
 
 app.use(express.json());
-// app.use(routes);
+app.use(equipamentoRoutes);
 // app.use(errorHandler);
 
 appDataSource.initialize()
