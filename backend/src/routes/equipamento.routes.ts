4c2ba274fc1ca0d37d3344be90a887ce2940b6ca
Mozart Silva <MozartSJ@st.corp>
2026-03-27 15:58:24 -0400

    feat: adicionado index.ts e routes

--- backend/src/routes/equipamento.routes.ts
+++ backend/src/routes/equipamento.routes.ts
@@ -8,7 +8,7 @@ router.post("/equipamentos", (req, res)=> controllers.create(req, res));
 
 
 router.get("/equipamentos", (req, res)=> controllers.list(req, res));
-router.put("/equipamentos/:codigo", (req, res)=> controllers.update(req, res)); 
+router.put("/equipamentos/:codigo", (req, res)=> controllers.update(req, res));
 
 
 export default router;
