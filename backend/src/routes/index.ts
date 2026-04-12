397a87eb01e4f1ffc3a3cd6c0d7245c7f24aeebb
Mozart Junior <mozartjrs@gmail.com>
2026-04-12 19:21:42 -0300

    feat: update authentication routes with validation and error handling; enhance app structure with error handler

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,6 +3,10 @@ import { EquipamentoController } from "../controllers/EquipamentoController.js";
 import { SetorController } from "../controllers/SetorController.js";
 import { UsuarioController } from "../controllers/UsuarioController.js";
 import { AuthController } from "../controllers/AuthController.js";
+import { ensureAuth } from "../middlewares/ensureAuth.js";
+import { validateBody } from "../middlewares/validateBody.js";
+import { loginSchema, refreshSchema, logoutSchema } from "../dtos/AuthDTO.js";
+import { createUserSchema } from "../dtos/createUserSchemaDTO.js";
 
 const routes = Router();
 const equipamentoController = new EquipamentoController();
@@ -17,27 +21,27 @@ routes.get("/", (req, res) => {
   });
 });
 
-// Auth
-routes.post("/auth/login", (req, res) => authController.login(req, res));
-routes.post("/auth/refresh", (req, res) => authController.refresh(req, res));
-routes.post("/auth/logout", (req, res) => authController.logout(req, res));
+// Auth (rotas públicas)
+routes.post("/auth/login", validateBody(loginSchema), (req, res, next) => authController.login(req, res, next));
+routes.post("/auth/refresh", validateBody(refreshSchema), (req, res, next) => authController.refresh(req, res, next));
+routes.post("/auth/logout", validateBody(logoutSchema), (req, res, next) => authController.logout(req, res, next));
 
-// Equipamentos
-routes.post("/equipamentos", (req, res) => equipamentoController.create(req, res));
-routes.get("/equipamentos", (req, res) => equipamentoController.getAll(req, res));
-routes.patch("/equipamentos/:id/desativar", (req, res) => equipamentoController.desativar(req, res));
-routes.get("/equipamentos/:id", (req, res) => equipamentoController.getById(req, res));
-routes.put("/equipamentos/:codigo", (req, res) => equipamentoController.update(req, res));
+// Equipamentos (rotas protegidas)
+routes.post("/equipamentos", ensureAuth, (req, res) => equipamentoController.create(req, res));
+routes.get("/equipamentos", ensureAuth, (req, res) => equipamentoController.getAll(req, res));
+routes.patch("/equipamentos/:id/desativar", ensureAuth, (req, res) => equipamentoController.desativar(req, res));
+routes.get("/equipamentos/:id", ensureAuth, (req, res) => equipamentoController.getById(req, res));
+routes.put("/equipamentos/:codigo", ensureAuth, (req, res) => equipamentoController.update(req, res));
 
-// Setores
-routes.post("/setores", (req, res) => setorController.create(req, res));
-routes.get("/setores", (req, res) => setorController.getAll(req, res));
-routes.patch("/setores/:id_setor/desativar", (req, res) => setorController.desativar(req, res));
-routes.get("/setores/:id_setor", (req, res) => setorController.getById(req, res));
+// Setores (rotas protegidas)
+routes.post("/setores", ensureAuth, (req, res) => setorController.create(req, res));
+routes.get("/setores", ensureAuth, (req, res) => setorController.getAll(req, res));
+routes.patch("/setores/:id_setor/desativar", ensureAuth, (req, res) => setorController.desativar(req, res));
+routes.get("/setores/:id_setor", ensureAuth, (req, res) => setorController.getById(req, res));
 
-// Usuários
-routes.post("/usuarios", (req, res) => usuarioController.create(req, res));
-routes.get("/usuarios", (req, res) => usuarioController.getAll(req, res));
-routes.get("/usuarios/:id_usuario", (req, res) => usuarioController.getById(req, res));
+// Usuários (rotas protegidas)
+routes.post("/usuarios", validateBody(createUserSchema), (req, res) => usuarioController.create(req, res));
+routes.get("/usuarios", ensureAuth, (req, res) => usuarioController.getAll(req, res));
+routes.get("/usuarios/:id_usuario", ensureAuth, (req, res) => usuarioController.getById(req, res));
 
 export default routes;
